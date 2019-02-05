42b5c3488085798c487658f066c93ec20e0eb5ac
tianle <tianlechen@gmail.com>
2019-02-05 15:40:56 -0500

    spark the features and orderbook updates

--- Book.py
+++ Book.py
@@ -1,72 +1,116 @@
+%pyspark
+
+
 #import pyspark as spark
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def orderbook(symbol, timestamp, venue='TSX'):
+    '''return pandas table of order book'''
+    s = '''SELECT
+            SUM(book_change) AS quantity, 
+            side, 
+            price
+        FROM orderbook_tsx 
+        WHERE symbol='%s' 
+            AND date_string='%s' 
+            AND time <= timestamp '%s'
+            AND venue = '%s'
+            AND price > 0
+            AND price < 99999
+        GROUP BY side, price 
+        ORDER BY price ASC'''
+    
+    sargs = (
+        symbol,
+        timestamp.split()[0],
+        timestamp,
+        venue)
+        
+    df = spark.sql(s % sargs)
+    
+    # save as temp table, return where qty>0
+    df.createOrReplaceTempView('orderbook')
+    df = spark.sql('SELECT * FROM orderbook WHERE quantity > 0')
+    
+    df = df.toPandas()
+    df['price'] = df['price'].astype(float)
+    df['quantity'] = df['quantity'].astype(float)
+        
+    return df
 
 
 class Book(object):
 
-    def __init__(self, df):
+    def __init__(self, symbol, timestamp, venue='TSX'):
         '''df has columns (quantity, side, price)
         takes some time to run, bu should only need to be done once.
         '''
-        self.df = df
+        self.df = orderbook(symbol, timestamp, venue)
 
         # make touch
-        askprx = self.df.filter(self.df.side == 'Sell')
-        bidprx = self.df.filter(self.df.side == 'Buy')
-        bestask = askprx.agg({'price': 'min'}).collect()[0]['min(price)']
-        bestbid = bidprx.agg({'price': 'max'}).collect()[0]['max(price)']
-        self.touch = (float(bestbid), float(bestask))
+        self.touch = (0, 99999)
+        if len(df) > 0:
+            bestbid = max(self.df.loc[self.df['side'] == 'Buy', 'price'])
+            bestask = min(self.df.loc[self.df['side'] == 'Sell', 'price'])
+            self.touch = bestbid, bestask
 
 
     def features(self, q=None):
-        '''return orderbook features (i.e. spread, midprice, bestbid, bestask)'''
+        '''return orderbook features'''
         bestbid, bestask = self.touch
         spread = bestask - bestbid
-        midprice = .5*(bestask + bestbid)
+        prxmid = .5*(bestask + bestbid)
+
+        isbuyattouch = np.logical_and(self.df['side'] == 'Buy', self.df['price'] >= bestbid)
+        qbuytouch = np.sum(self.df.loc[isbuyattouch, 'quantity'])
+        issellattouch = np.logical_and(self.df['side'] == 'Sell', self.df['price'] <= bestask)
+        qselltouch = np.sum(self.df.loc[issellattouch, 'quantity'])
+        prxweightedtouch = qselltouch*bestbid + qbuytouch*bestask
+        prxweightedtouch = prxweightedtouch/(qselltouch+qbuytouch)
 
         out = {'spread': spread, 
-               'midprice': midprice, 
                'bestbid': bestbid, 
-               'bestask': bestask}
+               'bestask': bestask,
+               'prxmid': prxmid,
+               'prxweightedtouch': prxweightedtouch}
 
         return out
 
 
-    def updatebook(self, ordersdf):
-        '''return new orderbook updated with ordersdf'''
+    def updatebook(self, bkchanges):
+        '''return new Book updated with bkchanges'''
 
-        # aggregate new orders to get change in orderbook
-        bookdelta = ordersdf.groupBy('side', 'price').agg({'book_change': 'sum'})
+        bkchanges = bkchanges.toPandas()
 
-        # join new orders with change qty to old orderbook
-        oldbk = self.df
-        newbk = oldbk.join(other=bookdelta, 
-                           on=(oldbk.price == bookdelta.price) & (oldbk.side == bookdelta.side),
-                           how='outer')
-
-        # rename colnames and update to get new orderbook
-        newbk = newbk.withColumnRenamed('quantity', 'qold')
-        newbk = newbk.withColumnRenamed('sum(book_change)', 'qdelta')
-        newbk = newbk.withColumn('quantity', newbk.qold + newbk.qdelta)
-        newbk = newbk.select([oldbk.side, oldbk.price, 'quantity'])
-        newbk = newbk.filter('quantity > 0').orderBy('price')
-        return Book(newbk)
+        if len(bkchanges) > 0:
+            oldbk = self.df
+            bkdfnew = oldbk.merge(bkchanges, on=['price', 'side'], how='outer').fillna(0)
+            bkdfnew = bkdfnew.rename(columns={'quantity': 'oldq'})
+            bkdfnew['quantity'] = bkdfnew['oldq'] + bkdfnew['sum(book_change)']
+            bkdfnew = bkdfnew.loc[bkdfnew['quantity'] > 0, :]
+            bkdfnew = bkdfnew.sort_values('price')
+            return Book(bkdfnew[['side', 'price', 'quantity']])
+        else:
+            return self
 
 
-
-    def depthview(self, midppct=.5):
+    def depthview(self, viewpct=.5, plot=True):
         '''return dict to construct depth view'''
-        features = self.features()
-        midp = features['midprice']
 
-        bk = self.toPandas()
+        features = self.features()
+        bestbid = features['bestbid']
+        bestask = features['bestask']
+
+        bk = self.df
         isbuy = bk['side'] == 'Buy'
         issell = bk['side'] == 'Sell'
 
-        plotlow, plothigh = (midppct, 1+midppct)
-        plotprice = np.logical_and(bk['price'] > plotlow*midp, bk['price'] < plothigh*midp)
-        bk = bk.loc[plotprice, :]
+        plotlow, plothigh = (viewpct*bestbid, (1.+viewpct)*bestask)
+        isplot = np.logical_and(bk['price'] > plotlow, bk['price'] < plothigh)
+        bk = bk.loc[isplot, :]
 
         buycum = np.cumsum(bk.loc[isbuy, 'quantity'].values[::-1])[::-1]
         sellcum = np.cumsum(bk.loc[issell, 'quantity'].values)
@@ -75,5 +119,11 @@ class Book(object):
                'bidq': buycum,
                'askp': bk.loc[issell, 'price'],
                'askq': sellcum}
+
+        if plot:
+            plt.plot(out['bidp'], out['bidq'], color='green', alpha=.5)
+            plt.plot(out['askp'], out['askq'], color='red', alpha=.5)
+            plt.title('Depth View')
+            plt.show()
         
         return out
---
+++ orders_agg.py
@@ -0,0 +1,77 @@
+#import pyspark as spark
+import pandas as pd
+
+from Book import Book
+from Orders import Orders
+
+symbol = 'TD'
+date_string = '2019-01-22'
+venue = 'TSX'
+
+# timestamp = '2019-01-23 09:30:00.000'
+timestamptrunc = 16
+tfmt = '%Y-%M-%D %H:%m:%s'
+tradingdt = pd.date_range(date_string+' 09:30', date_string+' 16:00', freq='1min')
+
+
+def dailyorders(symbol, date_string, venue, timestamptrunc):
+
+    s = '''SELECT
+            book_change, 
+            side, 
+            price,
+            reason,
+            CAST(LEFT(CAST(time AS STRING), %s) AS timestamp) AS time_discrete
+        FROM orderbook_tsx 
+        WHERE symbol='%s' 
+            AND date_string='%s' 
+            AND venue = '%s'
+            AND price > 0
+            AND price < 99999
+        ORDER BY time_discrete ASC'''
+
+    sargs = (
+        timestamptrunc,
+        symbol,
+        date_string,
+        venue)
+        
+    return spark.sql(s % sargs).cache()
+
+
+ordersday = dailyorders(symbol, date_string, venue, timestamptrunc)
+ordersday.show(5)
+
+orderstimes = ordersday.select('time_discrete').distinct().orderBy('time_discrete')
+orderstimes = orderstimes.toPandas()['time_discrete']
+orderstimes
+
+ordersday_t = ordersday.filter(ordersday.time_discrete == '2019-01-22 12:00:00')
+#ordersday_t = ordersday.filter(ordersday.time_discrete == tradingdt[0].strftime(tfmt))
+ordersday_t.show(5)
+
+# get all the book changes
+resl_ordersdf = [ordersday.filter(ordersday.time_discrete == tdt.strftime(tfmt)) for tdt in orderstimes]
+resl_bkch = map(lambda x: Orders(x).bkchange(), resl_ordersdf)
+
+# update to get all orderbooks
+bktemp = Book(symbol, timestamp)
+bklist = [bkini]
+for bkch in resl_bkch:
+    bktemp = bktemp.updatebook(bkch)
+    bklist.append(bktemp)
+
+resl_ordordbk = zip((resl_ordersdf, bklist[:-1]))
+
+
+# get all features
+# order features need orderbook touch as input
+
+def worker(x):
+    orderstemp, bktemp = x
+    bkfeaturestemp = bktemp.features()
+    touchval = (bkfeaturestemp['bestbid'], bkfeaturestemp['bestask'])
+    return {'orderbook': bkfeaturestemp,
+            'orders': Orders(orderstemp).features(touchval)}
+
+resl_features = map(worker, resl_ordordbk)
