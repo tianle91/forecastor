34e7a1a9bfd516683d400d876361ccfa53b62b0d
tianle <tianlechen@gmail.com>
2019-02-11 14:26:44 -0500

    Update features_book.py

--- features_book.py
+++ features_book.py
@@ -40,15 +40,17 @@ def orderbook(ordersdf, timestamp, verbose=0):
     return bk
 
 
-def orderinterval(ordersdf, timestamp0, timestamp1, verbose=0):
+def orderinterval(ordersdf, timestamp0, timestamp1=None, verbose=0):
     '''return orders between timestamp0 and timestamp1'''
-    if timestamp0 < timestamp1:
-        t0, t1 = utctimestamp(timestamp0), utctimestamp(timestamp1)
+    if timestamp1 is None:
+        s = '''time < '%s' ''' % (utctimestamp(timestamp0))
     else:
-        raise ValueError('not (timestamp0 < timestamp1)!')
-    filstr = '''time BETWEEN '%s' AND '%s' '''
-    df = ordersdf.filter(filstr % (t0, t1))
-
+        if not (timestamp0 < timestamp1):
+            raise ValueError('not (timestamp0 < timestamp1)!')
+        t0, t1 = utctimestamp(timestamp0), utctimestamp(timestamp1)
+        s = '''time BETWEEN '%s' AND '%s' ''' % (t0, t1)
+    
+    df = ordersdf.filter(s)
     if verbose > 0:
         print ('len:', df.count())
     return df
@@ -59,26 +61,33 @@ if __name__ == '__main__':
     symbol = 'TD'
     venue = 'TSX'
     date_string = '2019-02-04'
-    
-    freq = '1H'
-    #freq = '30min'
-    #freq = '5min'
-    #freq = '1min'
-    #freq = '500ms'
-    #freq = '1ms'
-    tradingtimes = pd.date_range(
-        start = pd.to_datetime(date_string + ' 09:30'),
-        end = pd.to_datetime(date_string + ' 16:30'),
-        freq = freq)
 
     dfday = dailyorders(symbol, date_string, venue)
     dfday.cache()
+
+    
+    #freq = '1H'
+    freq = '30min'
+    #freq = '5min'
+    #freq = '1min'
+    tradingtimes = pd.date_range(
+        start = pd.to_datetime(date_string + ' 09:30'),
+        end = pd.to_datetime(date_string + ' 16:00'),
+        tz = 'US/Eastern',
+        freq = freq)
+
 
     bkfeatures = {}
     for dt in tradingtimes:
         print ('doing dt:', dt)
         bkfeatures[dt] = Book(orderbook(dfday, dt).toPandas()).features()
 
+
     ordfeatures = {}
-    for dt in tradingtimes:
-        print ('doing dt:', dt)
+    dtprev = tradingtimes[0]
+    ordfeatures[dtprev] = None
+    for dt in tradingtimes[1:]:
+        print ('doing dtprev:%s dt:%s' % (dtprev, dt))
+        dftemp = orderinterval(dfday, dtprev, dt)
+        touchtemp = bkfeatures[dtprev]['bestbid'], bkfeatures[dtprev]['bestask']
+        ordfeatures[dt] = ordfn.features(dftemp, touchtemp)
