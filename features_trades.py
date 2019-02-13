e668b739174c16fb8b7b08a494bcc97e6f733b76
tianle <tianlechen@gmail.com>
2019-02-13 14:39:58 -0500

    trades features

--- features_trades.py
+++ features_trades.py
@@ -1,11 +1,13 @@
+import time
 import pandas as pd
 import numpy as np
+
 import sparkdfutils as utils
+import trades_functions as trxfn
 
 
 def dailytrades(symbol, date_string):
     '''return table of trades'''
-        
     s = '''SELECT
             time,
             trade_size AS quantity,
@@ -20,81 +22,28 @@ def dailytrades(symbol, date_string):
             AND date_string = '%s' 
             AND price > 0
         ORDER BY time ASC'''
-    
     sargs = (symbol, date_string)
     return spark.sql(s % sargs)
-
-
-def features(df):
-
-    df = df.groupBy('price').agg({'quantity': 'sum'}).toPandas()
-    prx = df['price'].astype(float)
-    qty = df['sum(quantity)']
-    wgt = qty / np.sum(qty)
-
-    if verbose > 0:
-        print ('prx:', prx)
-        print ('wgt:', wgt)
-
-    tradecount = len(prx)
-
-    mean = 0
-    stdev = 0
-    tradeq = 0
-    qtypertrade = 0
-
-    if tradecount > 0:
-        mean =  np.average(prx, weights=wgt)
-        stdev = np.average(np.power(prx, 2), weights=wgt)
-        stdev = np.sqrt(stdev - np.power(mean, 2))
-        tradeq = np.sum(qty)
-        qtypertrade = tradeq/tradecount
-
-    out = {'mean': mean, 
-           'stdev': stdev, 
-           'traded_count': tradecount, 
-           'traded_qty': tradeq, 
-           'meanq_pertrade': qtypertrade}
-
-    return out
 
 
 if __name__ == '__main__':
 
     symbol = 'TD'
-    venue = 'TSX'
     date_string = '2019-02-04'
+    venue = 'TSX'
+    freq = '30min'
+    tstart_string = '10:00'
+    tend_string = '12:00'
+    verbose = 2
 
-    dfday = dailytrades(symbol, date_string)
+    tradingtimes = utils.tradingtimes(date_string, 
+        tstart_string, tend_string, freq, tz='US/Eastern')
+
+    # get all transactions prior to tradingtimes[-1]
+    dfday = dailytrades(symbol, date_string, venue)
+    dfday = utils.subsetbytime(dfday, tradingtimes[-1])
     dfday.cache()
+    if verbose > 0:
+        print ('get dailyorders done in: %.2f norders: %d before: %s' %\
+            (time.time()-t0, dfday.count(), tend_string))
 
-    
-    #freq = '1H'
-    #freq = '30min'
-    #freq = '5min'
-    freq = '1min'
-    tradingtimes = pd.date_range(
-        start = pd.to_datetime(date_string + ' 09:30:01'),
-        end = pd.to_datetime(date_string + ' 16:00:01'),
-        tz = 'US/Eastern',
-        freq = freq)
-    print ('len(tradingtimes):', len(tradingtimes))
-
-
-    trxfeatures = {}
-    # no new trades for first timestamp
-    dtprev = tradingtimes[0]
-    trxfeatures[dtprev] = None
-
-    for dt in tradingtimes[1:]:
-        t0 = time.time()
-        dftemp = utils.subsetbytime(dfday, dtprev, dt)
-        ntrades = dftemp.count()
-        
-        trxfttemp = None
-        if ntrades > 0:
-            trxfttemp = features(dftemp)
-            
-        trxfeatures[dt] = trxfttemp
-        dtprev = dt
-        print ('dtprev:%s dt:%s, done in:%s' % (dtprev, dt, time.time()-t0))
---
+++ trades_functions.py
@@ -0,0 +1,54 @@
+import numpy as np
+import pandas as pd
+
+
+def features_gpbyprice(df):
+	'''return features of all trades grouped by price'''
+    if type(df) is not pd.DataFrame:
+        raise TypeError('df is not pd.DataFrame!')
+    elif not {'price', 'quantity'}.issubset(df.columns):
+    	raise ValueError('''['price', 'quantity'] not in df.columns!''')
+    else:
+        df = df.astype({'price': float, 'quantity': float})
+
+    ntrades = len(df)
+    dfgpbyprx = df.groupby('price')['quantity'].agg('sum')
+    print (dfgpbyprx)
+
+    prx = df['price']
+    qty = df['quantity']
+    wgt = qty / np.sum(qty)
+
+    mean = 0
+    stdev = 0
+    tradeq = 0
+    qtypertrade = 0
+
+    if tradecount > 0:
+        mean =  np.average(prx, weights=wgt)
+        stdev = np.average(np.power(prx, 2), weights=wgt)
+        stdev = np.sqrt(stdev - np.power(mean, 2))
+        tradeq = np.sum(qty)
+        qtypertrade = tradeq/tradecount
+
+    out = {'mean': mean, 
+           'sd': stdev, 
+           'Number-of-Trades': tradecount, 
+           'Quantity-Traded': tradeq, 
+           'AvgVol': qtypertrade}
+
+    return out
+
+
+def features_gpbybroker(df):
+	'''return features of all trades grouped by broker'''
+    if type(df) is not pd.DataFrame:
+        raise TypeError('df is not pd.DataFrame!')
+    elif not {'buy_broker', 'sell_broker'}.issubset(df.columns):
+    	raise ValueError('''['buy_broker', 'sell_broker'] not in df.columns!''')
+    pass
+
+
+def features(df):
+	out = {'gpbyprice': features_gpbyprice(df)}
+	return out
