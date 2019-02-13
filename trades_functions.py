e3aed040f91deb3907745bfd738b9cd22542d4ab
tianle <tianlechen@gmail.com>
2019-02-13 16:29:15 -0500

    to numpy and then increment in transfer matrix

--- trades_functions.py
+++ trades_functions.py
@@ -42,9 +42,7 @@ def filltrfm(aggdf, transfermatrix):
     m = transfermatrix.copy()
     for buy_broker, dfsub in aggdf.groupby(level=0):
         for sell_broker, dfsub1 in dfsub.groupby(level=1):
-            print (sell_broker, buy_broker)
-            print (dfsub1)
-            m.loc[sell_broker, buy_broker] += float(dfsub1)
+            m.loc[sell_broker, buy_broker] += float(dfsub1.to_numpy())
     return m
 
 
