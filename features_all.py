923d28728d0d68716cd4f79b01a3e94d25af6979
Tianle Chen <tianlechen@gmail.com>
2019-02-24 19:05:18 -0500

    updated with options

--- features_all.py
+++ features_all.py
@@ -6,17 +6,32 @@ import numpy as np
 import pandas as pd
 
 
-jobname = 'long'
-
-tstart_string = '10:00'
-tend_string = '11:00'
-freq = '1min'
+datelenname = '1wk'
+timelenname = '1h'
+tsunit = 'MINUTE'
 symbol = 'TD'
 
-dates = pd.date_range(
-    start = pd.to_datetime('2018-04-01'),
-    end = pd.to_datetime('2018-04-03') if jobname == 'short' else pd.to_datetime('2018-06-01'),
-    freq = 'B')
+
+# process arguments
+if timelenname == '1h':
+    tstart_string = '10:00'
+    tend_string = '11:00'
+elif timelenname == 'fullday':
+    tstart_string = '09:30'
+    tend_string = '16:00'
+
+if datelenname == '1wk':
+    start = pd.to_datetime('2018-04-01')
+    end = pd.to_datetime('2018-04-08')
+elif datelenname == '1mo':
+    start = pd.to_datetime('2018-04-01')
+    end = pd.to_datetime('2018-05-31')
+
+jobname = '%s-%s' % (datelenname, timelenname)
+
+
+# initialize parameters
+dates = pd.date_range(start = start, end = end, freq = 'B')
 print ('dates:', dates)
 
 
@@ -24,7 +39,7 @@ def getparams(dt, verbose):
     out = {
         'symbol': symbol,
         'date_string': dt.strftime('%Y-%m-%d'),
-        'freq': freq, 
+        'tsunit': tsunit, 
         'tstart_string': tstart_string, 
         'tend_string': tend_string, 
         'verbose': verbose,
@@ -33,7 +48,7 @@ def getparams(dt, verbose):
 
 
 t1 = time.time()
-exec(open('features_orders.py').read())
+exec(open('features_orders_gpbyagg.py').read())
 resdorders = {dt: features(**getparams(dt, verbose=1)) for dt in dates}
 print ('done in: %.2f' % (time.time()-t1))
 
