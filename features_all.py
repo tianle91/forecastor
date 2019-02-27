7df3c3d666e522bf4237693b0df71798161d119f
tianle <tianlechen@gmail.com>
2019-02-27 11:58:28 -0500

    arguments

--- features_all.py
+++ features_all.py
@@ -7,20 +7,6 @@ import numpy as np
 import pandas as pd
 
 
-tsunit = 'MINUTE'
-
-symbol = 'TD'
-#symbol = 'BPY.UN'
-#symbol = 'UFS'
-#symbol = 'VFV'
-
-#datelenname = '1wk'
-datelenname = '1mo'
-
-timelenname = '1h'
-#timelenname = 'fullday'
-
-
 # process arguments
 if timelenname == '1h':
     tstart_string = '10:00'
@@ -35,8 +21,6 @@ if datelenname == '1wk':
 elif datelenname == '1mo':
     start = pd.to_datetime('2018-04-01')
     end = pd.to_datetime('2018-05-31')
-
-jobname = '%s-%s' % (datelenname, timelenname)
 
 
 # initialize parameters
@@ -56,37 +40,57 @@ def getparams(dt, verbose):
     return out
 
 
-# ------------------------------------------------------------------------------
-# dump the dates
-# ------------------------------------------------------------------------------
-pickle.dump(dates, open(os.getcwd() + '/data/%s_SYM:%s_dates.pickle' % (jobname, symbol), 'wb'))
+if __name__ == '__main__':
 
 
-# ------------------------------------------------------------------------------
-# run the orders features
-# ------------------------------------------------------------------------------
-exec(open('features_orders_gpbyagg.py').read())
+    tsunit = 'MINUTE'
 
-def worker(dt, jobid, overwrite=True, verbose=1):
-    fname = os.getcwd() + '/data/%s_SYM:%s_dt:%s_orders.pickle.gz' % (jobname, symbol, dt)
-    if overwrite and not os.path.isfile(fname):
-        out = features(**getparams(dt, verbose=verbose))
-        pickle.dump(out, gzip.open(fname, 'wb'))
+    symbol = 'TD'
+    #symbol = 'BPY.UN'
+    #symbol = 'UFS'
+    #symbol = 'VFV'
 
-for dt in dates:
-    temp =  worker(dt, jobname)
+    #datelenname = '1wk'
+    datelenname = '1mo'
+
+    timelenname = '1h'
+    #timelenname = 'fullday'
 
 
-# ------------------------------------------------------------------------------
-# run the trades features
-# ------------------------------------------------------------------------------
-exec(open('features_trades_gpbyagg.py').read())
+    jobname = '%s-%s' % (datelenname, timelenname)
 
-def worker(dt, jobid, overwrite=True, verbose=1):
-    fname = os.getcwd() + '/data/%s_SYM:%s_dt:%s_trades.pickle.gz' % (jobname, symbol, dt)
-    if overwrite and not os.path.isfile(fname):
-        out = features(**getparams(dt, verbose=verbose))
-        pickle.dump(out, gzip.open(fname, 'wb'))
 
-for dt in dates:
-    temp =  worker(dt, jobname)
+    # ------------------------------------------------------------------------------
+    # dump the dates
+    # ------------------------------------------------------------------------------
+    pickle.dump(dates, open(os.getcwd() + '/data/%s_SYM:%s_dates.pickle' % (jobname, symbol), 'wb'))
+
+
+    # ------------------------------------------------------------------------------
+    # run the orders features
+    # ------------------------------------------------------------------------------
+    exec(open('features_orders_gpbyagg.py').read())
+
+    def worker(dt, jobid, overwrite=True, verbose=1):
+        fname = os.getcwd() + '/data/%s_SYM:%s_dt:%s_orders.pickle.gz' % (jobname, symbol, dt)
+        if overwrite and not os.path.isfile(fname):
+            out = features(**getparams(dt, verbose=verbose))
+            pickle.dump(out, gzip.open(fname, 'wb'))
+
+    for dt in dates:
+        temp =  worker(dt, jobname)
+
+
+    # ------------------------------------------------------------------------------
+    # run the trades features
+    # ------------------------------------------------------------------------------
+    exec(open('features_trades_gpbyagg.py').read())
+
+    def worker(dt, jobid, overwrite=True, verbose=1):
+        fname = os.getcwd() + '/data/%s_SYM:%s_dt:%s_trades.pickle.gz' % (jobname, symbol, dt)
+        if overwrite and not os.path.isfile(fname):
+            out = features(**getparams(dt, verbose=verbose))
+            pickle.dump(out, gzip.open(fname, 'wb'))
+
+    for dt in dates:
+        temp =  worker(dt, jobname)
